e85c5cce6ec94cf547aa0bcc694fe925c3975629
Emmanuel <emmanuelsibanda21@gmail.com>
2018-09-30 11:38:45 +0200

    Assignment
    Assignment2 (Data Science Specialization)

---
+++ cachematrix.R
@@ -0,0 +1,31 @@
+## creates a matrix that will be able to cache its inverse
+
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) inv <<- inverse
+  getInverse <- function() inv
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+}
+
+
+## computes the inverse of the above matrix
+
+cacheSolve <- function(x, ...) {
+  m <- x$getInverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setInverse(m)
+  m
+}
